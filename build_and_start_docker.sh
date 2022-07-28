66181d705e837fe4f9d5a0e692d827815c2845a3
echel0n <melih.sahin@protonmail.com>
2022-07-29 00:27:29 +0300

    fix naming

--- build_and_start_docker.sh
+++ build_and_start_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t brainflow . && 
-	docker run -dit --rm --cap-add=SYS_PTRACE --name brainoverflow --hostname brainoverflow --mount type=tmpfs,destination=/ramdisk -e AFL_TMPDIR=/ramdisk brainoverflow /bin/bash &&
-	docker exec -ti brainoverflow /bin/bash
+	docker run -dit --rm --cap-add=SYS_PTRACE --name brainflow --hostname brainflow --mount type=tmpfs,destination=/ramdisk -e AFL_TMPDIR=/ramdisk brainflow /bin/bash &&
+	docker exec -ti brainflow /bin/bash
